e4c66b04b3f4df82569f4d14d0cf74b6fb79e57d
Andrew Otto <acotto@gmail.com>
2015-06-30 12:17:03 -0400

    Revert previous changes for upgrade to v250.  v250 has bugs with log files.  242-1 worked fine, reverting to that.
    Also adding parameter to specify version.  This will be set to 242-1
    in operations/puppet
    
    Change-Id: I1d7c2a4254ed378f2562e26c81267c6d1f63d4d3

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,14 +6,16 @@
 # == Parameters
 # $run_interval - seconds between runs of jmx queries.  Default: 15
 # $log_level    - level at which to log jmxtrans messages.  Default: 'info'
+# $version      - version of jmxtrans to install.  Default: installed (no preference)
 #
 class jmxtrans(
     $run_interval = 15,
     $log_level    = 'info',
+    $version      = 'installed'
 )
 {
     package { 'jmxtrans':
-        ensure  => 'installed',
+        ensure  => $version,
     }
 
     file { '/etc/jmxtrans':
@@ -31,21 +33,5 @@ class jmxtrans(
         enable    => true,
         require   => Package['jmxtrans'],
         subscribe => File['/etc/default/jmxtrans'],
-    }
-
-    # TEMPORARY HACK.
-    # https://github.com/jmxtrans/jmxtrans/issues/215
-    # I cannot adjust log_level or configure log4j to
-    # propertly rotate files until we have a version
-    # of jmxtrans where this is fixed.  Remove all jmxtrans
-    # logs for now.  We don't really need these anyway.
-    cron { 'jmxtrans-log-purge':
-        # This is so awful.  The version of jmxtrans we have now
-        # seems to not respond to 'service jmxtrans stop'.
-        command => '/usr/bin/dpkg -s jmxtrans | /bin/grep -q "Version: 250" && (/usr/bin/pkill -f "/usr/bin/java.+jmxtrans-all.jar" && sleep 6; /bin/rm -r /var/log/jmxtrans/*.log*; /usr/sbin/service jmxtrans start)',
-        minute  => 0,
-        hour    => 0,
-        user    => 'root',
-        require => Service['jmxtrans'],
     }
 }
